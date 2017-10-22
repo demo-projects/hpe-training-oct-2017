5858efdd934fd04c2848b03e63be06f9e2a055fa
nirkaufman <nirkaufman@gmail.com>
2017-10-22 16:34:45 +0300

    basic data bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Welcome to app!</h1>
-  `
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-  public title = 'app';
+
+  public title: string;
+  public text: string;
+  public val: string;
+  public flag: boolean;
+
+  constructor() {
+    this.title = 'Hello Angular';
+    this.text = 'type me!';
+    this.val = '';
+    this.flag = true;
+  }
+
+  changeTitle() {
+    this.title = 'New Title';
+  }
+
 }
