0489d1a96b17b74cc82151762bdfb016d72da319
nirkaufman <nirkaufman@gmail.com>
2017-10-29 12:14:33 +0200

    inputs

--- src/app/todolist/todolist/todolist.component.ts
+++ src/app/todolist/todolist/todolist.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodolistComponent implements OnInit {
 
-  public title: string;
+  public appTitle: string;
   public items: Item[];
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
-    this.title = 'TOLOZ';
+    this.appTitle = 'TOLOZ';
     this.items = [new Item('js'), new Item('ts')];
   }
 
